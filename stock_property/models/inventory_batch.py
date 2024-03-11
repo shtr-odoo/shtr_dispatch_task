8870066cbce7d202415375df49052997142daef5
shtr-odoo <shtr@odoo.com>
2024-03-11 14:00:25 +0530

    gantt-changes

--- stock_property/models/inventory_batch.py
+++ stock_property/models/inventory_batch.py
@@ -12,20 +12,20 @@ class InventoryBatch(models.Model):
      volume = fields.Float(string="Volume", readonly=True, compute='_compute_weight_volume', store=True)
      no_line = fields.Integer(string="Lines" ,compute='_compute_transfer_line' ,store=True)
      no_transfer = fields.Integer(string="Transfer", compute='_compute_transfer_line' ,store=True)
+     total_w = fields.Float(string="Total Weight")
+     total_v = fields.Float(string="Total Volume")
 
      @api.depends('picking_ids','vehicle_category')
      def _compute_weight_volume(self):
-          temp_w = 0
-          temp_v = 0
           self.weight = 0
           self.volume = 0
 
           for record in self:
-               temp_w = sum((line.weight) for line in record.picking_ids)
-               temp_v = sum((line.volume) for line in record.picking_ids)
+               self.total_w = sum((line.weight) for line in record.picking_ids)
+               self.total_v = sum((line.volume) for line in record.picking_ids)
 
-          if self.vehicle_category.max_weight > 0 : self.weight = (temp_w / self.vehicle_category.max_weight)*100
-          if self.vehicle_category.max_volume > 0 : self.volume = (temp_v / self.vehicle_category.max_volume)*100
+          if self.vehicle_category.max_weight > 0 : self.weight = (self.total_w / self.vehicle_category.max_weight)*100
+          if self.vehicle_category.max_volume > 0 : self.volume = (self.total_v/ self.vehicle_category.max_volume)*100
 
      @api.depends('picking_ids','move_line_ids')
      def _compute_transfer_line(self):
@@ -34,3 +34,10 @@ class InventoryBatch(models.Model):
                batch.no_transfer = len(batch.picking_ids)
                # Counting the number of lines
                batch.no_line = sum(len(picking.move_line_ids) for picking in batch.picking_ids) 
+
+     
+
+     @api.depends('weight', 'volume')
+     def _compute_display_name(self):
+        for record in self:
+            record.display_name = record.name + " (" + str(format(record.weight,".2f")) + "kg, " + str(format(record.volume,".2f")) + "m3)"
